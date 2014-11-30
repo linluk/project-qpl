f7eed375cff135caebec06aa1949361f1d544767
lukas42singer@gmail.com <lukas42singer@gmail.com@71ced95a-5a0d-f81c-ece1-51e75f1dd83b>
2014-11-30 11:40:45 +0000

    License information in every source file. Added COPYING and README files. Added map.c and utils.c to edit.sh (to open ALL project files at once).

--- trunk/ast.h
+++ trunk/ast.h
@@ -1,3 +1,29 @@
+/*****************************************************************************
+ *
+ * Copyright (C) 2014 Lukas Singer
+ *
+ * This file is part of 'qpl'.
+ *
+ * 'qpl' is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * 'qpl' is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with 'qpl'.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The name 'qpl' stands for Quick Programming Language and is a working
+ * title.  It may changes in the future.
+ *
+ * Source code can be found under: <https://code.google.com/p/project-qpl/>.
+ *
+ ****************************************************************************/
+
 #ifndef __AST_H__
 #define __AST_H__
 
