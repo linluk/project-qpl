d701966241278923551055de20420f216de3eda2
Lukas Singer <lukas42singer (at) gmail dot com>
2016-02-10 21:33:02 +0100

    prepared examples for presentation

--- cat_files.sh
+++ cat_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# this script prints all the source files in the current directory.
-#
+# this script prints the content of all the source files in the
+# current directory.
 # you can use xclip to pipe the output to the clipboard so that it
 # can easily be pastet to whereever you want.
 #
@@ -15,16 +15,15 @@ echo_80() {
 }
 
 remove_double_newlines() {
+  # the sed command makes a single blank line from multiple
   sed -e '/./b' -e :n -e 'N;s/\n$//;tn'
 }
 
-for file in cat_files.sh README Makefile bnf *.h *.c *.l *.y
+for file in cat_files.sh Makefile *.h *.c *.l *.y
 do
-  echo_80 "#"
-  echo "Dateiname: $file"
-  echo_80 "#"
-  # the sed command makes a single blank line from multiple
+  echo_80 "="
+  echo "===  Dateiname: $file"
+  echo_80 "="
   cat $file | remove_double_newlines
-  echo ""
   echo ""
 done
